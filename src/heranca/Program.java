1f63e04f24f309cf725d76276c3fdae8c682e6ca
Leonardo Macedo <leo.depaula2019@gmail.com>
2023-03-02 12:00:50 -0300

    Iniciando estudos de herança e polimorfismo

---
+++ src/heranca/Program.java
@@ -0,0 +1,53 @@
+package heranca;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Account acc = new Account(1001, "Alex", 0.0);
+		acc.deposit(100.0);
+		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+		
+		// UPCASTING
+		
+		Account acc1 = bacc;
+		acc1.withdraw(50.0);
+		Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+		Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.1);
+		
+		// DOWNCASTING
+		
+		BusinessAccount acc4 = (BusinessAccount) acc2;
+		acc4.loan(100.0);
+		
+		if (acc3 instanceof BusinessAccount) {
+			BusinessAccount acc5 = (BusinessAccount)acc3;
+			acc5.loan(200.0);
+			System.out.println("Loan!");
+		}
+		
+		if (acc3 instanceof SavingsAccount) {
+			SavingsAccount acc5 = (SavingsAccount)acc3;
+			acc5.updateBalance();
+			System.out.println("Update");
+		}
+		
+		// Override
+		
+		System.out.println("-----------------");
+		Account acc6 = new Account(1001, "Alex", 1000.0);
+		acc6.withdraw(200.0);
+		System.out.println(acc6.getBalance());
+		
+		System.out.println("-----------------");
+		Account acc7 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
+		acc7.withdraw(200.0);
+		System.out.println(acc7.getBalance());
+		
+		System.out.println("-----------------");
+		Account acc8 = new BusinessAccount(1003, "Joao", 1000.0, 5000.0);
+		acc8.withdraw(200.0);
+		System.out.println(acc8.getBalance());
+	}
+
+}
